9295a3a736b760de08cd5270a1f83464e65c57f2
milo2112 <mjrp.ros@gmail.com>
2023-10-27 15:34:22 -0300

    requiring server to the app

---
+++ backend/src/server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+
+const PORT = process.env.PORT ?? 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(routes.users)
+app.use(routes.operations)
+app.use(routes.errors)
+
+app.all('*', (_, res) => res.status(404).json({ code: 404, message: 'Page Not Found' }))
+
+app.listen(PORT, () => console.log(`Server started at: http://localhost:${PORT}`))
+
+module.exports = app
