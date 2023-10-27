dce63ce06af5bf58fdcabc5d8975ac6e40586ea9
milo2112 <mjrp.ros@gmail.com>
2023-10-27 15:35:36 -0300

    importing routes from server

---
+++ backend/src/server/routes/index.js
@@ -0,0 +1,9 @@
+const users = require('./users.route')
+const operations = require('./operations.route')
+const errors = require('./errors.route')
+
+module.exports = {
+  users,
+  operations,
+  errors
+}
