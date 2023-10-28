fa12e67f7b4223fd719fc91643d41491f4d34548
milo2112 <mjrp.ros@gmail.com>
2023-10-28 19:30:18 -0300

    adding encryption logic

--- backend/src/server/models/Users.dao.js
+++ backend/src/server/models/Users.dao.js
@@ -1,17 +1,19 @@
 const { execQuery } = require('../databases/execQuery')
+const bcrypt = require('bcryptjs')
 
 const findUsers = async () => await execQuery('SELECT * FROM usuarios;')
 
-const findSingleUser = async (email) => await execQuery('SELECT * FROM usuarios WHERE email = $1;', [email])
+const findSingleUserFromDB = async (email) => await execQuery('SELECT * FROM usuarios WHERE email = $1;', [email])
 
-const saveUserRequest = async ({ email, password, rol, lenguage }) => {
+const saveUserToDB = async ({ email, password, rol, lenguage }) => {
+  const encryptPass = bcrypt.hashSync(password)
   const query = 'INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4) RETURNING *;'
-  const values = [email, password, rol, lenguage]
+  const values = [email, encryptPass, rol, lenguage]
   return await execQuery(query, values)
 }
 
 module.exports = {
   findUsers,
-  findSingleUser,
-  saveUserRequest
+  findSingleUserFromDB,
+  saveUserToDB
 }
