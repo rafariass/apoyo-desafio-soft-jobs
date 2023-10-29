9987b7c0f4df0e9022e8ea714355ecd4a46b5574
milo2112 <mjrp.ros@gmail.com>
2023-10-29 11:24:26 -0300

    adding "incorrect password" http code status

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -4,12 +4,13 @@ const HTTP_STATUS = {
   unauthorized: {
     code: 401,
     text: {
-      op1: 'There is no token to validate',
-      op2: 'Invalid token format',
-      op3: 'Invalid token'
+      op1: 'There is no token to validate.',
+      op2: 'Invalid token format.',
+      op3: 'Invalid token.'
     }
   },
   not_found: { code: 404, text: 'Resource not found.' },
+  incorrect_pasword: { code: 401, text: 'Incorrect pasword.' },
   internal_server_error: { code: 500, text: 'Internal server error.' }
 }
 
