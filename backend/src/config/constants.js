6baf421838625bd6c7a995532fcdd21463d2bfe0
milo2112 <mjrp.ros@gmail.com>
2023-10-28 23:01:32 -0300

    adding unauthorized error codes

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -1,6 +1,14 @@
 const HTTP_STATUS = {
   ok: { code: 200 },
   created: { code: 201 },
+  unauthorized: {
+    code: 401,
+    text: {
+      op1: 'There is no token to validate',
+      op2: 'Invalid token format',
+      op3: 'Invalid token'
+    }
+  },
   not_found: { code: 404, text: 'Resource not found.' },
   internal_server_error: { code: 500, text: 'Internal server error.' }
 }
