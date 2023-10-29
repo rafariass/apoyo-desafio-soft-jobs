3d51408363e2d1e98be5be30d9217278ad7d1d74
milo2112 <mjrp.ros@gmail.com>
2023-10-29 11:26:01 -0300

    adding http status 500 code

--- backend/src/server/controllers/users.controller.js
+++ backend/src/server/controllers/users.controller.js
@@ -1,6 +1,7 @@
 const daoSQL = require('../models/Users.dao')
 const { jwtSign, jwtVerify } = require('../../utils/jwt')
 const HTTP_STATUS = require('../../config/constants')
+const bcrypt = require('bcryptjs')
 
 const findSingleUserRequest = (req, res) => {
   const decoded = jwtVerify(req
@@ -11,8 +12,12 @@ const findSingleUserRequest = (req, res) => {
   )
   daoSQL.findSingleUserFromDB(decoded.email)
     .then((user) => {
+      // console.log('\ntype of user->', typeof user)
+      // console.log('\nuser como se enviaba antes->', user)
+      // console.log('\ndata enviada como arr->', [{ email: user[0].email, rol: user.rol, lenguage: user.lenguage }])
+      // console.log('\ndata enviada como obj->', user.email)
       user.length > 0
-        ? res.status(HTTP_STATUS.ok.code).json(user)
+        ? res.status(HTTP_STATUS.ok.code).json([{ email: user[0].email, rol: user[0].rol, lenguage: user[0].lenguage }])
         : res
           .status(HTTP_STATUS.not_found.code)
           .json(
@@ -22,10 +27,10 @@ const findSingleUserRequest = (req, res) => {
             }
           )
     })
-    .catch((error) => res.status(500).json(error))
+    .catch((error) => res.status(HTTP_STATUS.internal_server_error.code).json(error))
 }
 
-const authenticationRequest = (req, res) => {
+const authenticationRequest = async (req, res) => {
   daoSQL.verifyCredentials(req.body.email, req.body.password)
     .then((user) => {
       user.length > 0
